549959b52f0e79e2e0c92f8bebe1c37a059c3bf9
woter86 <woter86@gmail.com>
2025-01-16 17:02:14 +0300

    add eversense one minute

--- app/src/main/java/com/eveningoutpost/dexdrip/utils/DexCollectionType.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/utils/DexCollectionType.java
@@ -336,6 +336,8 @@ public enum DexCollectionType {
         switch (type) {
             case LibreReceiver:
                 return libreOneMinute ? 60_000 : 300_000;
+            case NSEmulator:
+                return 60_000;
             default:
                 return 300_000; // 5 minutes
         }
